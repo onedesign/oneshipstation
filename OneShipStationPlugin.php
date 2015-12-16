a4ed760e67eb4499cbd72267732f01a3392c5356
thelowlypeon <peter@onedesigncompany.com>
2015-12-16 11:20:58 -0600

    [#110263262] adds basic requirements to onBeforeInstall() in plugin

--- OneShipStationPlugin.php
+++ OneShipStationPlugin.php
@@ -20,7 +20,26 @@ class OneShipStationPlugin extends BasePlugin {
         return 'https://onedesigncompany.com';
     }
 
-    public function onBeforeInstall() {}
+    public function onBeforeInstall() {
+        // require Craft 2.5+
+        if (version_compare(craft()->getVersion(), '2.5', '<')) {
+            throw new Exception('One ShipStation requires Craft CMS 2.5+ in order to run.');
+        }
+
+        // require PHP 5.4+
+        if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50400) {
+            Craft::log('One ShipStation requires PHP 5.4+ in order to run.', LogLevel::Error);
+            return false;
+        }
+
+        // require Craft Commerce 1.0+
+        if (!($commerce = craft()->plugins->getPlugin('commerce')) || version_compare($commerce->getVersion(), '1.0', '<')) {
+            Craft::log('One ShipStation requires Craft Commerce 1.0+.', LogLevel::Error);
+            return false;
+        }
+
+        return true;
+    }
 
     public function onAfterInstall() {}
 
