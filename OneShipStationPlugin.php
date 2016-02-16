103059b54d2115ce87a4dc9ebcf970b8dff19fcb
thelowlypeon <peter@onedesigncompany.com>
2016-02-16 14:49:44 -0600

    updates version number

--- OneShipStationPlugin.php
+++ OneShipStationPlugin.php
@@ -9,7 +9,7 @@ class OneShipStationPlugin extends BasePlugin {
     }
 
     public function getVersion() {
-        return '0.2';
+        return '0.2.1';
     }
 
     public function getDeveloper() {
